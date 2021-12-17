747f633efee15de04e573bb8610bd33abbd6731f
Ashiq Legi <ashiqlegi@gmail.com>
2021-12-17 04:08:07 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return(`This Saturday, I want to ${fun}!`);
+    
+}
+
+function mondayWork(action = "go to the office") {
+    return(`This Monday, I will ${action}.`)
+}
+
+function wrapAdjective(pa = "*") {
+    return function inner(para="special") {
+        return(`You are ${pa}${para}${pa}!`)
+    }
+}
